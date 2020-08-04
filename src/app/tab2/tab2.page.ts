cb54a5a1fbf6e4554d5004c26b377eb4859a62b2
whiteKhoukha <38367409+whiteKhoukha@users.noreply.github.com>
2020-08-04 17:34:16 +0200

    first commit

---
+++ src/app/tab2/tab2.page.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { PhotoService } from '../services/photo.service';
+
+@Component({
+  selector: 'app-tab2',
+  templateUrl: 'tab2.page.html',
+  styleUrls: ['tab2.page.scss']
+})
+export class Tab2Page implements OnInit{
+
+  photos = this.photoService.photos;
+
+  constructor(public photoService: PhotoService) {}
+
+  ngOnInit() {
+    this.photoService.loadSaved();
+  }
+
+  addPhotoToGallery() {
+    this.photoService.addNewToGallery();
+  }
+
+}
